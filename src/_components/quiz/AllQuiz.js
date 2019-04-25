2972158e9ddcbe39d0e0d5e70c3e2be5fb5dd0e3
Léo Nicaisse <contact@leonicaisse.fr>
2019-04-25 10:01:21 +0200

    aouidheziulfberoiguernhf,iuzykdfgh,kuygn

--- src/_components/quiz/AllQuiz.js
+++ src/_components/quiz/AllQuiz.js
@@ -1,19 +1,40 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+
+import {Link} from 'react-router-dom';
+
+import {Row, Col} from 'reactstrap';
 
 class AllQuiz extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            //...
+            categories: [
+                'Culture',
+                'Histoire',
+                'Musique',
+                'Sciences',
+                'Cuisine',
+                'Animaux',
+                'Art',
+                'Cinema'
+            ],
         }
     }
 
     render() {
         return (
-            <div>
-                AllQuiz
-            </div>
+            <Row className={'px-1'}>
+                {this.state.categories.map((category, key) =>
+                    <Col xs={6} key={key} className={'text-center my-2 px-2'}>
+                        <Link to={'/quiz/' + category}>
+                            <div className={'bg-light rounded-lg p-5'}>
+                                {category.toUpperCase()}
+                            </div>
+                        </Link>
+                    </Col>
+                )}
+            </Row>
         )
     }
 }
--- src/_components/quiz/SingleQuiz.js
+++ src/_components/quiz/SingleQuiz.js
@@ -5,15 +5,269 @@ class SingleQuiz extends Component {
         super(props);
 
         this.state = {
-            //...
+            quiz: {
+                'Culture': {
+                    questions: [
+                        {
+                            'title': 'Question numéro 1 (culture)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 2 (culture)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 3 (culture)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        }
+                    ]
+                },
+                'Histoire': {
+                    questions: [
+                        {
+                            'title': 'Question numéro 1 (histoire)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 2 (histoire)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 3 (histoire)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        }
+                    ]
+                },
+                'Musique': {
+                    questions: [
+                        {
+                            'title': 'Question numéro 1 (musique)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 2 (musique)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 3 (musique)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        }
+                    ]
+                },
+                'Sciences': {
+                    questions: [
+                        {
+                            'title': 'Question numéro 1 (sciences)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 2 (sciences)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 3 (sciences)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        }
+                    ]
+                },
+                'Cuisine': {
+                    questions: [
+                        {
+                            'title': 'Question numéro 1 (cuisine)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 2 (cuisine)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 3 (cuisine)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        }
+                    ]
+                },
+                'Animaux': {
+                    questions: [
+                        {
+                            'title': 'Question numéro 1 (animaux)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 2 (animaux)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 3 (animaux)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        }
+                    ]
+                },
+                'Art': {
+                    questions: [
+                        {
+                            'title': 'Question numéro 1 (art)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 2 (art)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 3 (art)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        }
+                    ]
+                },
+                'Cinema': {
+                    questions: [
+                        {
+                            'title': 'Question numéro 1 (cinema)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 2 (cinema)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        },
+                        {
+                            'title': 'Question numéro 3 (cinema)',
+                            'answers': {
+                                1: 'Réponse fausse',
+                                2: 'Réponse fausse',
+                                3: 'Réponse fausse',
+                                4: 'Réponse vraie',
+                            }
+                        }
+                    ]
+                },
+            }
         }
     }
 
     render() {
-        let quizId = this.props.match.params.id;
+        let category = this.props.match.params.category;
         return (
             <div>
-                Single quiz (id = {quizId})
+                <p>Les questions du quiz {category} :</p>
+                <ul>
+                    {this.state.quiz[category].questions.map((question, key) =>
+                        <li key={key}>{question.title}</li>
+                    )}
+                </ul>
             </div>
         )
     }
